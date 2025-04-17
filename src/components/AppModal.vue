5b364bf78e728f34e2fbf11c6960415673093aa0
greg <gregory.tuck@hotmail.com>
2025-04-17 15:06:43 +0100

    Extract modal and tidy up

---
+++ src/components/AppModal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    const open = defineModel<boolean>();
+</script>
+
+<template>
+    <div v-if="open" class="z-50 fixed inset-0 flex justify-center items-center" @click.self="open = false">
+        <div class="fixed bg-black opacity-70 w-full h-full"></div>
+
+        <div class="z-50 bg-base-100 shadow-lg p-8 rounded">
+            <div class="mb-8">
+                <h2 class="font-bold text-white text-2xl">
+                    <slot name="title" />
+                </h2>
+
+                <h3 class="text-lg">
+                    <slot name="subtitle" />
+                </h3>
+            </div>
+
+            <slot />
+        </div>
+    </div>
+</template>
--- src/views/questions.vue
+++ src/views/questions.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+    import AppModal from '@/components/AppModal.vue';
     import AppTextbox from '@/components/AppTextbox.vue';
     import ExamDetails from '@/components/ExamDetails.vue';
     import ExamNotFound from '@/components/ExamNotFound.vue';
@@ -102,55 +103,40 @@
             @change="debouncedSaveExam"
         />
 
-        <!-- mark exam modal -->
-        <div
-            v-if="showMarkExamModal"
-            class="z-50 fixed inset-0 flex justify-center items-center"
-            @click.self="showMarkExamModal = false"
-        >
-            <div class="fixed bg-black opacity-70 w-full h-full"></div>
+        <AppModal v-model="showMarkExamModal">
+            <template #title>Mark Exam</template>
+            <template #subtitle>
+                Are you ready to complete this exam and have it marked? If so, upload the mark scheme and/or any marking
+                guidelines you have.
+            </template>
 
-            <div class="z-50 space-y-4 bg-gray-500 shadow-lg p-8 rounded">
-                <div>
-                    <h2 class="mb-2 text-2xl">Mark Exam</h2>
-                    <p>
-                        Are you ready to complete this exam and have it marked? If so, upload the mark scheme and/or any
-                        marking guidelines you have.
-                    </p>
-                </div>
+            <div class="space-y-2">
+                <input
+                    type="file"
+                    class="input"
+                    accept=".pdf"
+                    @change="(e: Event) => (uploadedMarkScheme = (e.target as HTMLInputElement).files?.[0] || null)"
+                />
 
-                <div class="space-y-2">
-                    <input
-                        type="file"
-                        class="bg-gray-700 px-3 py-2 border border-red-500 focus:border-red-500 rounded focus:outline-none w-full text-light-100 placeholder-green-500"
-                        accept=".pdf"
-                        placeholder="Upload PDF"
-                        @change="(e: Event) => (uploadedMarkScheme = (e.target as HTMLInputElement).files?.[0] || null)"
-                    />
-
-                    <AppTextbox v-model="markGuidelines" :placeholder="'Marking guidelines...'" :rows="5" />
-                </div>
-
-                <div class="flex justify-end space-x-2 mt-4">
-                    <button
-                        type="button"
-                        class="bg-red-500 px-4 py-2 rounded w-full text-white cursor-pointer"
-                        @click="showMarkExamModal = false"
-                    >
-                        I'm not finished, go back
-                    </button>
-                    <button
-                        type="button"
-                        class="bg-green-500 px-4 py-2 rounded w-full text-white cursor-pointer"
-                        :class="marking ? 'opacity-50 cursor-not-allowed' : ''"
-                        :disabled="marking"
-                        @click="markExam"
-                    >
-                        My answers are final, mark my exam
-                    </button>
-                </div>
+                <AppTextbox v-model="markGuidelines" :placeholder="'Marking guidelines...'" :rows="5" />
             </div>
-        </div>
+
+            <div class="flex justify-center space-x-2 mt-4">
+                <button type="button" class="flex-grow btn btn-primary" @click="showMarkExamModal = false">
+                    I'm not finished, go back
+                </button>
+
+                <button
+                    type="button"
+                    class="flex-grow btn btn-error"
+                    :class="marking ? 'opacity-50 cursor-not-allowed' : ''"
+                    :disabled="marking"
+                    @click="markExam"
+                >
+                    My answers are final, mark my exam
+                </button>
+            </div>
+        </AppModal>
     </form>
 
     <ExamNotFound v-else class="mt-16" />
